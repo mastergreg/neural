9c8596f3667bc87edb2bd1507853e353b02cce91
Gregory Liras <gregliras@gmail.com>
2012-09-01 19:51:13 +0300

    getting stuff done

--- Askisi 1/ask1.m
+++ Askisi 1/ask1.m
@@ -29,10 +29,36 @@ EditedTrainDataTargets = EditedTrainDataTargets(:, suffle);
 
 [TrainRemoved, settings] = removeconstantrows(EditedTrainData);
 
-%[TestRemoved, settings] = removeconstantrows('apply', TestData, settings);
+[TestRemoved, settings] = removeconstantrows('apply', TestData, settings);
+
+
 
 [TrainRemoved, ps] = processpca(TrainRemoved, 0.001);
 
-%[TestRemoved, ps] = processpca('apply', TestRemoved, ps);
+[TestRemoved, ps] = processpca('apply', TestRemoved, ps);
+
+%evaled_DATA = [];
+for i = 1:6,
+    for j = 1:6,
+        j*5
+        net = newff(TrainRemoved, EditedTrainDataTargets, [5*i 5*j]);
+        net.divideParam.trainRatio = 0.8;
+        net.divideParam.valRatio = 0.2;
+        net.divideParam.testRatio = 0;
+
+        net.trainParam.epochs = 300;
+        net = train(net, TrainRemoved, EditedTrainDataTargets);
+        TestDataOutput = sim(net, TestRemoved);
+        a = eval_Accuracy_Precision_Recall(TestDataOutput, TestDataTargets);
+        evaled_DATA(i,j,:) = a;
+    end
+end
+
+
+
+
+
+
+
 
 
